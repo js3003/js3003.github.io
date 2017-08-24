51f294c499de7505150009f228ed493166e4f03f
Irina <irbris@mail.ru>
2017-08-24 18:39:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var inputs = document.querySelectorAll("input");
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
+function handleUpdate() {
+    var suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
